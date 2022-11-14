432a800306bbdb277eecd011d51f304e160f2cb5
olechochek <olenkaevstratova@gmail.com>
2022-11-14 21:24:15 +0300

    full gallery

---
+++ js/galery.js
@@ -0,0 +1,11 @@
+new Swiper(".mySwiper", {
+    spaceBetween: 90,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    mousewheel: true,
+    keyboard: true,
+    slidesPerView:3,
+});
