ff9e9fcfd523e1e24160d4c5168e77b274749a01
olechochek <olenkaevstratova@gmail.com>
2022-11-26 12:28:56 +0300

    fix adapt Galery

--- js/galery.js
+++ js/galery.js
@@ -22,3 +22,9 @@ new Swiper(".mySwiper", {
         }
     }
 });
+let menuBtn=document.querySelector('.header_burger');
+let menu=document.querySelector('.header_list')
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active')
+})
