bb3a81eb8f7b2cd4b82e1f883d2bb13c403ae442
olechochek <olenkaevstratova@gmail.com>
2022-11-26 21:54:53 +0300

    test3

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log(window.screen.height)
+let menuBtn=document.querySelector('.header_burger');
+let menu=document.querySelector('.header_list')
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active')
+})
